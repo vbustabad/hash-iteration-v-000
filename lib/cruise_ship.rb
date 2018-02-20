17e045082970a8e9dd35d99d2a47fe65b55576a1
Veronica Bustabad <vbustabad@gmail.com>
2018-02-20 01:57:35 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -6,8 +6,8 @@
 # suite_e: "Crumpet the Elf"
 # }
 
-winner = ""
 def select_winner(passengers)
+winner = ""
   passengers.each do |suite, name|
     if suite == :suite_a && name.start_with?("A")
       winner = name
